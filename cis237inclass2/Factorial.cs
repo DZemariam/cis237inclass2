e143a521f550b9f56eb801814f5bc4f0107aada9
Dahwheat <Davidzemariam@gmail.com>
2017-01-25 20:43:44 -0500

    With your help fixed wineItem Class, UserInterface Class & some of WineItemCollection Class

---
+++ cis237inclass2/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass2
+{
+    class Factorial
+    {
+        public int CalculateFactorial (int number)
+        {
+            //Base Case
+            if (number == 1)
+            {
+                return number;
+            }
+            //Not base case, recursive call our self.
+            return number * CalculateFactorial(number - 1);
+        }
+    }
+}
